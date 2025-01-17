37340f3dda5c9ad1f8214772755e77cc085fce4b
James Unekwuojo <unekwuojoabah4@gmail.com>
2025-01-17 14:59:05 +0000

    Save progress: 360.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-echo -e  "\n~~ Questionnaire ~~\n"
+
+echo -e "\n~~ Questionnaire ~~\n"
+
 QUESTION1="What's your name?"
-
-echo $QUESTION1
-
-read NAME
-
-echo Hello $NAME.
-
 QUESTION2="Where are you from?"
-
-echo $QUESTION2
-
-read LOCATION
-
-echo Hello $NAME from $LOCATION.
-
 QUESTION3="What's your favorite coding website?"
 
-echo $QUESTION3
+echo $QUESTION1
+read NAME
 
+echo $QUESTION2
+read LOCATION
+
+echo $QUESTION3
 read WEBSITE
+
+echo Hello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!
+
