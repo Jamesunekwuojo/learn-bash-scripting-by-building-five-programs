46372021b3f74b6a50d8e20c04191bff34c2dd18
James Unekwuojo <unekwuojoabah4@gmail.com>
2025-01-19 13:24:43 +0000

    Save progress: 600.1

--- countdown.sh
+++ countdown.sh
@@ -3,15 +3,6 @@
 # Program that counts down to zero from a given argument
 
 if [[ $1 -gt 0 ]]
-
-then
-  echo true
-
-else
-  echo Include a positive interger as the first argument.
-
-fi
-if [[ $1 -gt 0 ]]
 then
   echo true
 else
