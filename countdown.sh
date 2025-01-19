67dcd420ac29162888da549e4e113ecd3ed8534f
James Unekwuojo <unekwuojoabah4@gmail.com>
2025-01-19 14:33:32 +0000

    created bingo sh

--- countdown.sh
+++ countdown.sh
@@ -2,10 +2,18 @@
 
 # Program that counts down to zero from a given argument
 
+echo -e "\n~~ Countdown Timer ~~\n"
+
 if [[ $1 -gt 0 ]]
 then
+  : '
+  for (( i = $1; i >= 0; i-- ))
+  do
+    echo $i
+    sleep 1
+  done
+  '
 
-  echo -e "\n~~ Countdown Timer ~~\n"
   I=$1
 
   while [[ $I -ge 0 ]]
@@ -14,20 +22,6 @@ then
     (( I-- ))
     sleep 1
   done
-    
- : '
-  for (( i = $1; i >= 0; i-- ))
-  do
-    echo $i
-    sleep 1
-  done
-  '
 else
   echo Include a positive integer as the first argument.
 fi
-
-
-
-
-
-
